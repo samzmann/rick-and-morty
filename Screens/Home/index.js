e37b9442a6247732d0ee2ae3c642c8bcd46eaeab
Samuel Mann <samuelcmann@gmail.com>
2019-09-30 21:59:32 +0200

    fix linter, cleanup boiler plate

---
+++ Screens/Home/index.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react'
+import { SafeAreaView, Text } from 'react-native'
+
+const Home = () => (
+  <SafeAreaView>
+    <Text>Welcome Home!</Text>
+  </SafeAreaView>
+)
+
+export default Home
