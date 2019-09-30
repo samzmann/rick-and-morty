e12239365fbaa8f59b018ccf5e0c01ee6aa29305
Samuel Mann <samuelcmann@gmail.com>
2019-09-30 22:19:43 +0200

    fetch data and render basic list of characters

--- Screens/Home/index.js
+++ Screens/Home/index.js
@@ -1,10 +1,47 @@
 import React, { useState, useEffect } from 'react'
-import { SafeAreaView, Text } from 'react-native'
+import { SafeAreaView, Text, FlatList, View } from 'react-native'
+import axios from 'axios'
 
-const Home = () => (
-  <SafeAreaView>
-    <Text>Welcome Home!</Text>
-  </SafeAreaView>
-)
+const characterEndpoint = 'https://rickandmortyapi.com/api/character/'
+
+const Home = () => {
+  const [characters, setCharacters] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        console.log('fetching data')
+        const { data } = await axios.get(characterEndpoint)
+        console.log(data)
+        setCharacters(data.results)
+      } catch (error) {
+        console.log('Error fetching data:', error)
+      }
+    }
+
+    fetchData()
+  }, [])
+
+  const keyExtractor = (item, index) => index
+
+  return (
+    <SafeAreaView>
+      <Text>Welcome Home!</Text>
+      <FlatList
+        keyExtractor={keyExtractor}
+        data={characters}
+        extraData={characters}
+        renderItem={({ item }) => {
+          console.log(item)
+          return (
+            <View>
+              <Text>{item.name}</Text>
+            </View>
+          )
+        }}
+      />
+    </SafeAreaView>
+  )
+}
 
 export default Home
